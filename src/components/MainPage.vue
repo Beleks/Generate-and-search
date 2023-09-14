3d00ba54c43d06912b8ea33252f436523e878545
BeleckijAV <aleksey.beletsky.av@gmail.com>
2023-09-14 12:15:00 +0700

    Added data saving and loading; improved generation

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -35,12 +35,16 @@ export default {
   },
   data() {
     return {
-      totalStrings: 10000000,
+      totalStrings: 1000000,
       searchTerm: "",
       strings: [],
       displayedStrings: [],
       currentPage: 1,
       resultsPerPage: 30,
+      dbConfig: {
+        name: "testDB",
+        objName: "strings",
+      },
     };
   },
   computed: {
@@ -51,7 +55,7 @@ export default {
       const startIndex = (this.currentPage - 1) * this.resultsPerPage;
       const endIndex = startIndex + this.resultsPerPage;
 
-      return this.strings.slice(startIndex, endIndex);
+      return this.displayedStrings.slice(startIndex, endIndex);
     },
     progress() {
       return (this.strings.length / this.totalStrings) * 100;
@@ -59,42 +63,32 @@ export default {
   },
   methods: {
     generateStrings() {
-      // Подход к реализации генерации и сохранения не лучший :(
-
-      // Наверное, лучше бы это делать партиями, скажем, по 1000, если нам важна скорость.
-      // Альтернативный вариант сохранения: Событие покидание страницы можно отловить и перед этим все записать в LS.
-      // Но если мы закроем страницу, не находясь на ней, то у нас ничего не сохранится.
-      // Поэтому выбрал такой вариант исходя из условий тестового
-
-      // Возможно, к моменту проверки я все-таки доделаю вариант с использованием Web Workers, в другой ветке. Так, для эксперимента )
-      localStorage.setItem("inProgress", true);
       let i = this.strings.length;
+      const batchSize = 1000;
 
-      let generationInterval;
-      generationInterval = setInterval(() => {
-        if (i < this.totalStrings) {
-          const randomString = this.generateRandomString(100);
-          this.strings.push(randomString);
+      localStorage.setItem("inProgress", true);
 
-          if (this.searchTerm.length) {
-            this.performSearch();
-          }
+      const generationInterval = setInterval(() => {
+        const batchData = this.generateBatchData(batchSize);
+        this.strings = this.strings.concat(batchData);
 
-          // В любом случае у нас не получится сохранить 10 миллионов строк на клиенте. Даже если использовать indexDB
-          try {
-            localStorage.setItem("data", JSON.stringify(this.strings));
-          } catch (error) {
-            clearInterval(generationInterval);
-            alert(`Ошибка при сохранения в LocalStorage: ${error}`);
-            return;
-          }
+        i += batchSize;
+        this.saveDataToIndexedDB(batchData);
 
-          i++;
-        } else {
+        if (i >= this.totalStrings) {
+          // Не корректно. Строки могут быть сгенерированы, но не сохранены.
+          // TODO: Стек вызовов saveDataToIndexedDB можно передать в Promise.all и по завершению удалять ключ InProgress
           localStorage.removeItem("inProgress");
           clearInterval(generationInterval);
         }
       }, 0);
+    },
+    generateBatchData(batchSize) {
+      const batchData = [];
+      for (let i = 0; i < batchSize; i++) {
+        batchData.push(this.generateRandomString(100));
+      }
+      return batchData;
     },
     generateRandomString(length) {
       const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -108,26 +102,105 @@ export default {
       return result;
     },
     performSearch() {
-      this.displayedStrings = this.strings.filter((result) => result.includes(this.searchTerm));
+      if (this.searchTerm.length) {
+        // TODO: debounce тоже неплохо бы добавить
+        this.displayedStrings = this.strings.filter((result) => result.includes(this.searchTerm));
+      } else {
+        this.displayedStrings = [];
+      }
     },
-    loadResults() {},
+    saveDataToIndexedDB(strings) {
+      const request = window.indexedDB.open(this.dbConfig.name, 1);
+
+      request.onerror = function (event) {
+        console.error("Ошибка открытия базы данных", event.target.error);
+      };
+
+      request.onupgradeneeded = function (event) {
+        const db = event.target.result;
+        if (!db.objectStoreNames.contains(this.dbConfig.objName)) {
+          db.createObjectStore(this.dbConfig.objName, { keyPath: "id", autoIncrement: true });
+        }
+      }.bind(this);
+
+      request.onsuccess = function (event) {
+        const db = event.target.result;
+        const transaction = db.transaction(this.dbConfig.objName, "readwrite");
+        const objectStore = transaction.objectStore(this.dbConfig.objName);
+
+        let addDataRequest;
+
+        for (const string of strings) {
+          addDataRequest = objectStore.add({ value: string });
+        }
+
+        addDataRequest.onsuccess = function () {
+          console.log("Данные успешно добавлены в IndexedDB");
+        };
+
+        addDataRequest.onerror = function (event) {
+          console.error("Ошибка при добавлении данных", event.target.error);
+        };
+
+        transaction.oncomplete = function () {
+          db.close();
+        };
+
+        transaction.onerror = function (event) {
+          console.error("Ошибка", event.target.error);
+        };
+      }.bind(this);
+    },
+    loadDataFromIndexedDB() {
+      const request = window.indexedDB.open(this.dbConfig.name, 1);
+
+      request.onerror = function (event) {
+        console.error("Ошибка открытия базы данных", event.target.error);
+      };
+
+      request.onupgradeneeded = function (event) {
+        const db = event.target.result;
+        if (!db.objectStoreNames.contains(this.dbConfig.objName)) {
+          db.createObjectStore(this.dbConfig.objName, { keyPath: "id", autoIncrement: true });
+        }
+      }.bind(this);
+
+      request.onsuccess = function (event) {
+        const db = event.target.result;
+        console.log(db.objectStoreNames, "db");
+        const transaction = db.transaction(this.dbConfig.objName, "readonly");
+        const objectStore = transaction.objectStore(this.dbConfig.objName);
+
+        const getDataRequest = objectStore.getAll();
+
+        getDataRequest.onsuccess = function (event) {
+          if (event.target.result && event.target.result.length > 0) {
+            this.strings = event.target.result.map((item) => item.value);
+            console.log("Данные успешно загружены из IndexedDB");
+          } else {
+            console.log("Данные в IndexedDB не найдены");
+          }
+          if (localStorage.getItem("inProgress")) {
+            this.generateStrings();
+          }
+        }.bind(this);
+
+        getDataRequest.onerror = function (event) {
+          console.error("Ошибка при чтении данных", event.target.error);
+        };
+
+        db.close();
+      }.bind(this);
+    },
+    setStrings(strings) {
+      this.strings = strings;
+    },
     changePage(newPage) {
       this.currentPage = newPage;
     },
   },
   mounted() {
-    const savedData = localStorage.getItem("data");
-
-    if (savedData) {
-      try {
-        this.strings = JSON.parse(savedData);
-      } catch (error) {
-        alert(`Ошибка при загрузке данных из LocalStorage: ${error}`);
-      }
-    }
-    if (localStorage.getItem("inProgress")) {
-      this.generateStrings();
-    }
+    this.loadDataFromIndexedDB();
   },
 };
 </script>
